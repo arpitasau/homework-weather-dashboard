d3f22dff54090277f10f893564742eba9fb1c77b
Arpita K Sau <arpitakaran@gmail.com>
2020-06-15 19:43:51 -0500

    added-forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,75 @@
+$("#display").hide();
 $(document).ready(function(){
+    var storedCity = [];
+    var cityName = JSON.parse(localStorage.getItem("city"));
+
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (cityName !== null) {
+    storedCity = cityName;
+  }
+    
     var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
-    var queryUrlForcast = "https://api.openweathermap.org/data/2.5/onecall?";
-    var appID = "0a336261969aedd3330885cbe45f26cc";
+    var queryUrlForcast = "http://api.openweathermap.org/data/2.5/onecall?lat=";
     $(".btn").on("click", function(){
-        var latitude, longitude;
-       
         $("#error").text('');
-        var city = $("#input-city").val();
+        var city = $("#input-city").val().toUpperCase();
+        $("#list").append("<button id='btn-city' >" +city+ "</button>");
+        localStorage.setItem("cityName", JSON.stringify(city));
+        storedCity.push(city);
+        $("#input-city").value = "";
+        console.log(city);
         //console.log(city);
         if(city != ''){
+            $("#display").show();
+            
+
             $.ajax({
                 url : queryUrl + city + "&units=imperial" + "&APPID=" + appID,
                 method : "GET",   
             }).then(function(response){
                 console.log(response);
-                $("#city-name").text(response.name);
-                $("#temperature").text(response.main.temp);
-                $("#wind-speed").text(response.wind.speed);
-                $("#humidity").text(response.main.humidity);
+                $("#city-name").text(response.name + " " + moment().format("(MM/DD/YYYY)"));
+                $("#temperature").text(response.main.temp + " " + "F");
+                $("#wind-speed").text(response.wind.speed + " " + "MPH");
+                $("#humidity").text(response.main.humidity + " " + "%");
                 $("#description").text(response.weather[0].description);
-                latitude = response.coord.lat;
+                var latitude = response.coord.lat;
                 console.log(latitude);
-                longitude = response.coord.lon;
+                var longitude = response.coord.lon;
                 console.log(longitude);
+
+                $.ajax({
+                    url : queryUrlForcast + latitude + "&lon=" + longitude  + "&units=imperial" + "&APPID=" + appID,
+                    method : "GET",   
+                }).then(function(responsefc){
+                    console.log(responsefc);
+                    $("#uv").text(responsefc.current.uvi);
+                    
+                    $("#date1").text(moment().add(1,'day').format("MM/DD/YYYY"))
+                    $("#date2").text(moment().add(2,'day').format("MM/DD/YYYY"))
+                    $("#date3").text(moment().add(3,'day').format("MM/DD/YYYY"))
+                    $("#date4").text(moment().add(4,'day').format("MM/DD/YYYY"))
+                    $("#date5").text(moment().add(5,'day').format("MM/DD/YYYY"))
+                    
+                    $("#temp1").text(responsefc.daily[0].temp.day + " " + "F");
+                    $("#temp2").text(responsefc.daily[1].temp.day + " " + "F");
+                    $("#temp3").text(responsefc.daily[2].temp.day + " " + "F");
+                    $("#temp4").text(responsefc.daily[3].temp.day + " " + "F");
+                    $("#temp5").text(responsefc.daily[4].temp.day + " " + "F");
+
+                    $("#hum1").text(responsefc.daily[0].humidity+ " " + "%");
+                    $("#hum2").text(responsefc.daily[1].humidity + " " + "%");
+                    $("#hum3").text(responsefc.daily[2].humidity+ " " + "%");
+                    $("#hum4").text(responsefc.daily[3].humidity + " " + "%");
+                    $("#hum5").text(responsefc.daily[4].humidity + " " + "%");
+
+                    $("#img1").attr("src", "http://openweathermap.org/img/w/" + responsefc.daily[0].weather[0].icon + ".png")
+                    $("#img2").attr("src", "http://openweathermap.org/img/w/" + responsefc.daily[1].weather[0].icon + ".png")
+                    $("#img3").attr("src", "http://openweathermap.org/img/w/" + responsefc.daily[2].weather[0].icon + ".png")
+                    $("#img4").attr("src", "http://openweathermap.org/img/w/" + responsefc.daily[3].weather[0].icon + ".png")
+                    $("#img5").attr("src", "http://openweathermap.org/img/w/" + responsefc.daily[4].weather[0].icon + ".png")
+                })
             })
-            
-            $.ajax({
-                url : queryUrlForcast + latitude + "&units=imperial" + "&APPID=" + appID,
-                method : "GET",   
-            }).then(function(response){
-                console.log(response);
-                
-            })
-//             https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&
-// exclude=hourly,daily&appid={YOUR API KEY}
-            // $.ajax({
-            //     url : "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=" + appID,
-            //     method : "GET",   
-            // }).then(function(response){
-            //     console.log(response);
-                
-            // })
             
         }
         else{
@@ -48,24 +77,5 @@ $(document).ready(function(){
         }
      
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 })
